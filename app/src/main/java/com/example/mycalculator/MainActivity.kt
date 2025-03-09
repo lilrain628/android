16e98da7474a600a2847b6aa10d334aaee692835
lilrain628 <vova.melentif@yandex.ru>
2025-03-10 01:08:39 +0700

    cyclic calc

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -1,20 +1,24 @@
 package com.example.mycalculator
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
+import android.widget.EditText;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity
 import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
-    lateinit var mathOperation: TextView
+    lateinit var mathOperation: TextView     //contains link to textview
     lateinit var resultText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mathOperation = findViewById<View>(R.id.math_operation) as TextView
+        mathOperation = findViewById<View>(R.id.math_operation) as TextView //find elements in the xml
         resultText = findViewById<View>(R.id.result_text) as TextView
 
         findViewById<View>(R.id.btn_0).setOnClickListener { v: View? ->
@@ -47,6 +51,7 @@ class MainActivity : AppCompatActivity() {
         findViewById<View>(R.id.btn_9).setOnClickListener { v: View? ->
             mathOperation.append("9")
         }
+
         findViewById<View>(R.id.dot_btn).setOnClickListener { v: View? ->
             mathOperation.append(".")
         }
@@ -63,16 +68,11 @@ class MainActivity : AppCompatActivity() {
         findViewById<View>(R.id.div_btn).setOnClickListener { v: View? ->
             mathOperation.append("/")
         }
-        findViewById<View>(R.id.lsk_btn).setOnClickListener { v: View? ->
-            mathOperation.append("(")
-        }
-        findViewById<View>(R.id.rsk_btn).setOnClickListener { v: View? ->
-            mathOperation.append(")")
-        }
+
 
         findViewById<View>(R.id.ac_btn).setOnClickListener { v: View? ->
             mathOperation.text = ""
-            resultText!!.text = ""
+            resultText.text = ""
         }
 
         findViewById<View>(R.id.back_btn).setOnClickListener { v: View? ->  //Проверяем, есть ли хотя бы один символ (text.length > 0).
@@ -85,10 +85,65 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<View>(R.id.equal_btn).setOnClickListener { v: View? ->
             try {
-                val result = ExpressionBuilder(mathOperation.text.toString()).build().evaluate()
-                resultText!!.text = result.toString()
+//                val result = ExpressionBuilder(mathOperation.text.toString()).build().evaluate()
+//                val lenght = mathOperation.text.toString().length
+//                resultText!!.text = result.toString()
+//                Log.d ("melentev", resultText.toString())
+//                println(result)
+//                //println(lenght)
+//                Log.d ("длина выражения", mathOperation.text.toString().length.toString())
+                val expression = mathOperation.text.toString()
+                val numbers = mutableListOf<Double>()
+                val operators = mutableListOf<Char>()
+                var currentNumber = ""
+                var i = 0
+                while (i < expression.length) {
+                    val ch = expression[i]  // Берём символ из строки
+
+                    if (ch.isDigit() || ch == '.') {
+                        currentNumber += ch  // Если цифра или '.', добавляем к текущему числу
+                    } else {
+                        if (currentNumber.isNotEmpty()) {
+                            numbers.add(currentNumber.toDouble())  // Если число накопилось, добавляем его в список
+                            currentNumber = ""  // Обнуляем
+                        }
+                        operators.add(ch)  // Добавляем оператор (+, -, *, /)
+                    }
+                    i++
+                }
+                if (currentNumber.isNotEmpty()) numbers.add(currentNumber.toDouble())
+
+
+                var j = 0                                                    // Шаг 1: Обрабатываем *, /
+                while (j < operators.size) {
+                    if (operators[j] == '*' || operators[j] == '/') {
+                        val left = numbers[j]
+                        val right = numbers[j + 1]
+                        val result = if (operators[j] == '*') left * right else left / right
+
+                        numbers[j] = result
+                        numbers.removeAt(j + 1)
+                        operators.removeAt(j)
+                        j--
+                    }
+                    j++
+                }
+
+                // Шаг 2: Обрабатываем +, -
+                var result = numbers[0]
+                for (k in operators.indices) {
+                    when (operators[k]) {
+                        '+' -> result += numbers[k + 1]
+                        '-' -> result -= numbers[k + 1]
+                    }
+                }
+                resultText.text = result.toString()
+
+
+
             } catch (e: Exception) {
                 resultText!!.text = "Ошибка"
+                Log.d ("oshibka!!!", resultText.toString())
             }
         }
     }
